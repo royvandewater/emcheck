2d7c1acd7b1200da42d24695b175a0eaa28dbb63
Jade Meskill and Roy van de Water <dev+jade+roy@integrumtech.com>
2013-12-25 09:18:24 -0700

    Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emcheck::Application.config.secret_key_base = 'cabf6a1ba239e2f3d85670d32e50f90f5703c1ca1699c532d64364e6645aedd9555c51e7d0b71af4f1e738b9482c4d0e44f72c2ee69b55ce3c4c7b7089dc6335'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emcheck::Application.config.session_store :cookie_store, key: '_emcheck_session'
